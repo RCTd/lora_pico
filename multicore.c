e5f64ea67259042d4dd607b3f087cb3379c31d2e
rafytudu <rafytudu@example.com>
2026-05-08 13:31:35 +0300

    125mhz with dma

--- multicore.c
+++ multicore.c
@@ -1,96 +1,67 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
-
 #include "hardware/pio.h"
-#include "hardware/clocks.h"
+#include "hardware/dma.h"
 #include "blink.pio.h"
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq);
+#define OUTPUT_PIN 1
+#define BUFFER_SIZE 1024
 
-// by default flash leds on gpios 3-4
-#ifndef PIO_BLINK_LED1_GPIO
-#define PIO_BLINK_LED1_GPIO 1
-#endif
+uint32_t data_buffer[BUFFER_SIZE];
 
-#define FLAG_VALUE 123
+void setup_data(uint32_t *buffer, size_t len) {
+    for (size_t i = 0; i < len; ++i) {
+        // Alternating 1s and 0s: 0xAAAAAAAA is 10101010...
+        buffer[i] = 0xAAAAAAAA;
+    }
+}
 
 void core1_entry() {
-  multicore_fifo_push_blocking(FLAG_VALUE);
-  uint32_t g = multicore_fifo_pop_blocking();
+    printf("Core 1: Setting up PIO and DMA\n");
 
-  if (g != FLAG_VALUE)
-    printf("Hmm, that's not right on core 1!\n");
-  else
-    printf("Its all gone well on core 1!");
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &lora_out_program);
+    uint sm = pio_claim_unused_sm(pio, true);
 
+    // Initialize PIO with no clock division (max speed)
+    lora_out_program_init(pio, sm, offset, OUTPUT_PIN, 1.0f);
 
-  printf("\nClock Hz: %d\n",clock_get_hz(clk_sys));
+    int dma_chan = dma_claim_unused_channel(true);
+    dma_channel_config c = dma_channel_get_default_config(dma_chan);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
+    channel_config_set_read_increment(&c, true);
+    channel_config_set_write_increment(&c, false);
+    channel_config_set_dreq(&c, pio_get_dreq(pio, sm, true));
 
-  assert(PIO_BLINK_LED1_GPIO < 31);
+    // For continuous output, we can use two DMA channels to chain each other,
+    // but for now, we'll just trigger it once or loop in code.
+    
+    while (1) {
+        dma_channel_configure(
+            dma_chan,
+            &c,
+            &pio->txf[sm], // Write to PIO TX FIFO
+            data_buffer,   // Read from our buffer
+            BUFFER_SIZE,   // Number of 32-bit words
+            true           // Start immediately
+        );
 
-  PIO pio[2];
-  uint sm[2];
-  uint offset[2];
-
-  // Find a free pio and state machine and add the program
-  bool rc = pio_claim_free_sm_and_add_program_for_gpio_range(&blink_program, &pio[0], &sm[0], &offset[0], PIO_BLINK_LED1_GPIO, 2, true);
-  hard_assert(rc);
-  printf("Loaded program at %u on pio %u\n", offset[0], PIO_NUM(pio[0]));
-
-  // Claim the next state machine and start led2 flashing
-  pio_sm_claim(pio[0], sm[0] + 1);
-  // Claim the next state machine and start led3 flashing
-  pio_sm_claim(pio[0], sm[0] + 2);
-
-  // Start led1 flashing
-//  blink_pin_forever(pio[0], sm[0], offset[0], PIO_BLINK_LED1_GPIO, 62400000);
-  blink_program_init(pio[0], sm[0], offset[0], PIO_BLINK_LED1_GPIO);
-  pio_sm_set_enabled(pio[0], sm[0], true);
-  // PIO counter program takes 3 more cycles in total than we pass as
-  // input (wait for n + 1; mov; jmp)
-  pio[0]->txf[sm[0]] = 0;
-
-//  blink_pin_forever(pio[0], sm[0] + 1, offset[0], PIO_BLINK_LED1_GPIO + 2, 1);
-//  blink_pin_forever(pio[0], sm[0] + 2, offset[0], PIO_BLINK_LED1_GPIO + 3, 62400000);
-
-  pio_sm_unclaim(pio[0], sm[0] + 1);
-  pio_sm_unclaim(pio[0], sm[0] + 2);
-  pio_remove_program_and_unclaim_sm(&blink_program, pio[0], sm[0], offset[0]);
-
-  // the program exits but the pio keeps running!
-  printf("All leds should be flashing\n");
-
-//  while (1)
-//    tight_loop_contents();
+        dma_channel_wait_for_finish_blocking(dma_chan);
+        // Small delay if we don't want to swamp the bus, though PIO will throttle via DREQ
+    }
 }
 
 int main() {
-  stdio_init_all();
-  printf("Hello, multicore!\n");
+    stdio_init_all();
+    printf("Pico W LoRa DMA Output Example\n");
 
-  multicore_launch_core1(core1_entry);
+    setup_data(data_buffer, BUFFER_SIZE);
 
-  uint32_t g = multicore_fifo_pop_blocking();
+    multicore_launch_core1(core1_entry);
 
-  if (g != FLAG_VALUE)
-    printf("Hmm, that's not right on core 0!\n");
-  else {
-    multicore_fifo_push_blocking(FLAG_VALUE);
-    printf("It's all gone well on core 0!");
-  }
-
-  while (1)
-    tight_loop_contents();
-}
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-  blink_program_init(pio, sm, offset, pin);
-  pio_sm_set_enabled(pio, sm, true);
-
-  printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-  // PIO counter program takes 3 more cycles in total than we pass as
-  // input (wait for n + 1; mov; jmp)
-  pio->txf[sm] = (clock_get_hz(clk_sys) / (2 * freq)) - 1;
+    while (1) {
+        printf("Core 0 is alive and handling USB...\n");
+        sleep_ms(1000);
+    }
 }
